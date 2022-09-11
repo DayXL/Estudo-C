fef9a6250d5eecb0035ea7dd82978401d9a3b939
Day <107324462+DayXL@users.noreply.github.com>
2022-09-11 14:33:43 -0300

    Criado funcao para ano bissexto

--- validarData.c
+++ validarData.c
@@ -7,18 +7,39 @@ int main(void) {
     int dia;
     int mes;
     int ano;
+    int a;
 
     printf("Digite o dia: ");
     scanf("%d", &dia);
     getchar();
 
     printf("Digite o mês: ");
-    scanf("%d", &dia);
+    scanf("%d", &mes);
     getchar();
 
     printf("Digite o ano: ");
-    scanf("%d", &dia);
+    scanf("%d", &ano);
     getchar();
 
+    a = anoBissexto(ano);
+
+    if (a == 1) {
+        printf("Ano bissexto");
+    }
+
+    else {
+        printf("Ano não bissexto");
+
+    }
+
+}
+
+int anoBissexto(int ano) {
+    int a;
+
+    //Caso ele siga a expressão nos parenteses retornara 1, caso não, irá retorna 0.
+    a = (((ano % 4 == 0) && (ano % 100 != 0)) || (ano % 400 == 0)) ? 1 : 0;
+
+    return a;
 
 }
