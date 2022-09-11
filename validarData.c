5f6a10087d4f9568691211eaf6b85d8c88b59133
Day <107324462+DayXL@users.noreply.github.com>
2022-09-11 14:17:29 -0300

    Criado estrutura principal

---
+++ validarData.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int dataValida(int, int, int);
+int anoBissexto(int);
+
+int main(void) {
+    int dia;
+    int mes;
+    int ano;
+
+    printf("Digite o dia: ");
+    scanf("%d", &dia);
+    getchar();
+
+    printf("Digite o mês: ");
+    scanf("%d", &dia);
+    getchar();
+
+    printf("Digite o ano: ");
+    scanf("%d", &dia);
+    getchar();
+
+
+}
