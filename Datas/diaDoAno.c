9cebe1f0b88c4d7c6d70bd608a14aee81c3e327f
Day <107324462+DayXL@users.noreply.github.com>
2022-09-21 17:27:50 -0300

    Criado base

--- Datas/diaDoAno.c
+++ Datas/diaDoAno.c
@@ -1,3 +1,43 @@
 #include <stdio.h>
 
 int diaDoAno(int, int, int);
+
+int main(void) {
+    int dia;
+    int mes;
+    int ano;
+    int a = 0;
+
+    while (a == 0) {
+
+        printf("Digite o dia: ");
+        scanf("%d", &dia);
+        getchar();
+
+        printf("Digite o mês: ");
+        scanf("%d", &mes);
+        getchar();
+
+        printf("Digite o ano: ");
+        scanf("%d", &ano);
+        getchar();
+
+        a = dataValida(dia, mes, ano);
+
+        if (a == 1) {
+            printf("\n");
+
+
+        }
+
+        else {
+            printf("\n");
+
+        }
+
+    }
+
+    printf("Programa encerrado");
+
+}
+
