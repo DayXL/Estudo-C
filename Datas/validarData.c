bb8166a4e6f2697e9d320686a098695368054e10
Day <107324462+DayXL@users.noreply.github.com>
2022-09-21 17:31:13 -0300

    assinando as funcoes no cabecalho

--- Datas/validarData.c
+++ Datas/validarData.c
@@ -1,10 +1,7 @@
 #include <stdio.h>
+#include "cabecalhoData.h"
 
 // Atividade de validar data
-
-int dataValida(int, int, int);
-int anoBissexto(int);
-int escolha(void);
 
 int main(void) {
     int dia;
