7dbc035ee78e92981f39af57a8d4723c43947562
DayXL <day.luc.xav0606@gmail.com>
2022-10-16 13:29:09 -0300

    criado struct cliente

--- usando struct/modCliente.h
+++ usando struct/modCliente.h
@@ -3,3 +3,15 @@ void validarNomeCliente(void);
 void validarCPF(void);
 void validarNumeroCelular(void);
 void validarEmail(void);
+
+typedef struct cliente Cliente;
+
+struct cliente {
+
+    char nomeCliente[100];
+    char cpf[12];
+    char numero[12];
+    char email[100];
+
+};
+
