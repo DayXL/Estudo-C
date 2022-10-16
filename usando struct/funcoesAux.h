302c2add47dcfa2137d9e574fe92ed663cdec83b
DayXL <day.luc.xav0606@gmail.com>
2022-10-16 12:19:58 -0300

    assinando funÇões

--- usando struct/funcoesAux.h
+++ usando struct/funcoesAux.h
@@ -1,2 +1,6 @@
 void passarEnter(void);
 char auxEscolha(void);
+int validarPalavra(char *palavra);
+int validarNumInteiro(char *numero);
+int verificarCpf(char *cpf);
+int verificarEmail(char *email, int tam);
