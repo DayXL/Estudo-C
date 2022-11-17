a2119970aa3c204b40482b285bc5ff2fbf0684de
DayXL <day.luc.xav0606@gmail.com>
2022-11-17 16:20:28 -0300

    código para analisar tabela do titanic

---
+++ Analisando Dados/analiseDados.c
@@ -0,0 +1,68 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+typedef struct titanic Titanic;
+
+struct Titanic {
+    char id[4];
+    char sobr;
+    char clas;
+    char nome[255];
+    char sex;
+    char idade[3];
+    int parents;
+    char bilhet[50];
+    float tarifa;
+    char cabine[20];
+    char embarcou;
+
+    Titanic* prox;
+
+};
+
+void lerArq(void);
+
+int main(void) {
+    lerArq();
+
+    return 0;
+
+}
+
+
+void lerArq(void) {
+    FILE *fp;
+    char letra;
+
+    fp = fopen("titanic.csv","rt");
+
+    if (fp == NULL){
+        printf("Erro ao abrir o arquivo\n!");
+
+    }
+
+    else {
+        
+        //De Flavius
+
+        letra = fgetc(fp);
+
+        while (letra != EOF) {
+
+            if (letra == ',') {
+                printf("\n");
+
+            }
+            else {
+                printf("%c", letra);
+
+            }
+
+            letra = fgetc(fp);
+
+        }
+
+    }
+
+}
